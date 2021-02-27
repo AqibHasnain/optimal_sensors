75f941908383bfc09c976636e9375acae6ba9cfb
Aqib <aqib@ucsb.edu>
2021-02-26 19:59:23 -0800

    server test

--- run.sh
+++ run.sh
@@ -4,15 +4,17 @@ rm log.txt
 
 # the inputs to main.py are the replicates to run the optimizations with. 
 # python3 main.py [0,1,2] run0 #| tee -a log.txt
-python3 main.py [0,1,2] run1 #| tee -a log.txt
-python3 main.py [0,1,2] run2 #| tee -a log.txt
-# python3 main.py [0,1,2] run3 #| tee -a log.txt
-# python3 main.py [0,1,2] run4 #| tee -a log.txt
-# python3 main.py [0,1,2] run5 #| tee -a log.txt
-# python3 main.py [0,1,2] run6 #| tee -a log.txt
-# python3 main.py [0,1,2] run7 #| tee -a log.txt
-# python3 main.py [0,1,2] run8 #| tee -a log.txt
-# python3 main.py [0,1,2] run9 #| tee -a log.txt
+python3 main.py [0,1,2] run1 | tee -a log.txt
+python3 main.py [0,1,2] run2 | tee -a log.txt
+python3 main.py [0,1,2] run3 | tee -a log.txt
+python3 main.py [0,1,2] run4 | tee -a log.txt
+python3 main.py [0,1,2] run5 | tee -a log.txt
+python3 main.py [0,1,2] run6 | tee -a log.txt
+python3 main.py [0,1,2] run7 | tee -a log.txt
+python3 main.py [0,1,2] run8 | tee -a log.txt
+python3 main.py [0,1,2] run9 | tee -a log.txt
+python3 main.py [0,1,2] run10 | tee -a log.txt 
+python3 main.py [0,1,2] run11 | tee - log.txt
 
 # first argument is the reps to use and the second is the horizon for optimization 
 # python3 main.py [0] 2 0
